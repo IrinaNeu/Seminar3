3974daff4ee9b1c7a965135f13177f812bc13aea
IrinaNeu <120553867+IrinaNeu@users.noreply.github.com>
2022-12-20 21:12:04 +0700

    Seminar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая принимает на вход число(N)
+// и выдает таблицу квадратов чисел от 1 до N.
+
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32 (Console.ReadLine());
+
+
+// for (int i=1;i<=num1;i++)
+// {
+//     Console.Write(i*i+",");
+// }
+//_________________________________________________
+
+// Задача 19 Написать программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+// int num = new Random().Next(100000);
+// Console.WriteLine(num);
+
+// String aNum = Convert.ToString(num);
+
+// if (aNum[0]==aNum[4]&&aNum[1]==aNum[3])
+// {
+//     Console.WriteLine("Число является палиндромом");
+// }
+// else 
+// {
+//     Console.WriteLine("Число не является палиндромом");
+// }
+//_____________________________________________________
+
+// // Задача 21 Написать программу, которая принимает на вход коордтинаты двух точек
+// //и находит расстояние между ними в 3D пространстве.
+
+// Console.WriteLine("Введите X для точки 1");
+// int X1 = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Введите Y для точки 1");
+// int Y1 = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Введите Z для точки 1");
+// int Z1 = Convert.ToInt32 (Console.ReadLine());
+
+
+// Console.WriteLine("Введите X для точки 2");
+// int X2 = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Введите Y для точки 2");
+// int Y2 = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Введите Z для точки 2");
+// int Z2 = Convert.ToInt32 (Console.ReadLine());
+
+
+// double d = Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
+// Console.WriteLine(d);
+//________________________________________________________________
+
+// Задача 23 Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N
+
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32 (Console.ReadLine());
+
+// for (int i=1;i<=num1;i++)
+// {
+//     Console.WriteLine(i*i*i);
+// }
+
